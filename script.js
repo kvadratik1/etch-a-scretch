e0afd12c95169f6b467f6c71cd3c4799bc55c48e
Sasha <2010kvadratik@gmail.com>
2025-05-04 15:31:30 +0300

    add function createBoard

---
+++ script.js
@@ -0,0 +1,11 @@
+const containerCell = document.querySelector("#container")
+const numberOfCells = 16 
+
+function createBoard (){
+   const area = numberOfCells * numberOfCells
+   for (let i = 0; i < area; i++){
+
+   const newCell = document.createElement("div")
+   
+   }
+}
