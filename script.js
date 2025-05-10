a04ffb2f3c832a8e8aab8b49d76ad6e6bbe8e4ce
Sasha <2010kvadratik@gmail.com>
2025-05-10 21:38:15 +0300

    makes grid but needs to finish

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const containerCell = document.querySelector("#container")
 const numberOfCells = 16 
 
-function createBoard (){
+function createBoard () {
    const area = numberOfCells * numberOfCells
-   for (let i = 0; i < area; i++){
+   for (let i = 0; i < area; i++) {
 
    const newCell = document.createElement("div")
-   
-   }
+   newCell.classList.add("cell");
+   newCell.addEventListener("mouseover", () => {
+   newCell.style.backgroundColor = "black"
+   })
+   containerCell.appendChild(newCell)
+  }
+
+   containerCell.style.display = "grid";
+   containerCell.style.gridTemplateColumns = `repeat(${numberOfCells}, 1fr)`;
+   containerCell.style.gridTemplateRows = `repeat(${numberOfCells}, 1fr)`;
 }
+ createBoard(16)
