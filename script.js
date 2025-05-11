75914acc5d43bc54cb6e6279c7f57bbaddf31cc9
Sasha <2010kvadratik@gmail.com>
2025-05-11 12:56:39 +0300

    make working 16x16 grid (only black color)

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const containerCell = document.querySelector("#container")
 const numberOfCells = 16 
+let isDrawing = false
 
-function createBoard () {
+function createBoard (numberOfCells) {
    const area = numberOfCells * numberOfCells
    for (let i = 0; i < area; i++) {
 
    const newCell = document.createElement("div")
    newCell.classList.add("cell");
    newCell.addEventListener("mouseover", () => {
-   newCell.style.backgroundColor = "black"
+      if (isDrawing){
+         newCell.style.backgroundColor = "black"
+      }
+      newCell.addEventListener("mousedown", () => {
+         isDrawing = true
+         newCell.style.backgroundColor = "black";
+       })
    })
    containerCell.appendChild(newCell)
   }
@@ -17,4 +24,9 @@ function createBoard () {
    containerCell.style.gridTemplateColumns = `repeat(${numberOfCells}, 1fr)`;
    containerCell.style.gridTemplateRows = `repeat(${numberOfCells}, 1fr)`;
 }
+
+
+document.addEventListener("mouseup", () => {
+   isDrawing = false
+})
  createBoard(16)
